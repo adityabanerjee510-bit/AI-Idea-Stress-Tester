df1bd90a852aa964c49538f8e6b42ee054f8914b
adityabanerjee510-bit <adityabanerjee510@gmail.com>
2026-03-25 22:49:25 +0530

    login and signup component added

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from fastapi import FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel, Field, computed_field
 from typing import List, Optional,Annotated,Literal
 import json
-
 app = FastAPI()
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://adityabanerjee510_db_user:<passward>@pymongo-1.johx7kf.mongodb.net/?appName=Pymongo-1"
+uri = f"mongodb+srv://adityabanerjee510_db_user:<password>@pymongo-1.johx7kf.mongodb.net/?appName=Pymongo-1"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -19,3 +18,7 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the AI Idea Stress Tester API!"}
